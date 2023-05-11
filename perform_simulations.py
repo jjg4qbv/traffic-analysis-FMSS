e3f7768575ec47ac5a98f77cac804e6f5431c526
Justin Guo <jjg4qbv@virginia.edu>
2023-05-11 14:17:55 -0400

    sims

--- perform_simulations.py
+++ perform_simulations.py
@@ -2,9 +2,12 @@ import subprocess
 
 num_sim = 200
 for i in range(0, num_sim):
-    cmd = ["C:/Program Files/prism-games-3.1/bin/prism.bat",
+    cmd = ["./../prism-games/prism/bin/prism",
            "./corner.prism",
            "-simpath",
            "1000",
-           "./sims/sim" + str(num_sim) + ".txt"]
-    subprocess.run(cmd, shell=True)
+       #     "stdout"
+           "./sims/sim" + str(200 + i) + ".txt"
+           ]
+    
+    subprocess.run(cmd)
